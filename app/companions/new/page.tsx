e089017cf5670386ae44638f4696fcb5e27f68b1
Wisdom Iwunwa <wisdomiwunwa@gmail.com>
2025-05-27 14:46:03 +0100

    saas_app

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewCompanion = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewCompanion
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  Page = () => {
+  return (
+    <div>
+      Sign In
+    </div>
+  )
+}
+
+export default Page
